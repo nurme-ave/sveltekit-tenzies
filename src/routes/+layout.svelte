7e935ffa63927fed989c6696e22ae97c9c2a3bc5
Ave Nurme <nurme.ave@gmail.com>
2024-11-15 21:00:13 +0200

    Continue working on the layout, add functionality to generate dice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
   let { children } = $props();
 </script>
 
-<main class="grid min-h-screen place-content-center bg-[#8A2BE2]">
+<main class="grid min-h-screen place-content-center bg-[#8A2BE2] font-poppins">
   {@render children()}
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,45 @@
+<script>
+  let dice = $state(
+    Array(10)
+      .fill()
+      .map(() => generateNewDie())
+  );
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    };
+  }
+</script>
+
 <section
-  class="flex max-w-[768px] flex-col gap-3 rounded-lg border border-gray-300 bg-[#F5F5F5] px-16 py-12 text-center">
-  <h1 class="text-5xl font-semibold uppercase tracking-wider text-[#8A2BE2]">Tenzies</h1>
-  <div>
+  class="flex max-w-[768px] flex-col gap-8 rounded-lg border border-gray-300 bg-[#F5F5F5] px-16 py-12 text-center">
+  <div class="space-y-0.5">
+    <h1 class="mb-4 text-5xl font-bold uppercase tracking-wide text-[#8A2BE2]">Tenzies</h1>
     <p class="text-lg">Roll until all dice are the same.</p>
     <p class="text-lg">Click each die to freeze it at its current value between rolls.</p>
+  </div>
+  <div class="text-xl font-semibold text-[#087533]">
+    <p>Rolls: 0</p>
+    <p>Best score: 0</p>
+  </div>
+  <div class="grid grid-cols-5 grid-rows-2 place-content-center gap-4">
+    {#each dice as die}
+      <div
+        class="flex w-full cursor-pointer items-center justify-center rounded-lg border-t-2 px-4 py-6 text-2xl text-black shadow-md">
+        {die.value}
+      </div>
+    {/each}
+  </div>
+  <div class="flex flex-row items-center justify-center gap-6">
+    <button
+      aria-label="reset game"
+      class="w-[7em] rounded-lg bg-red-500 px-4 py-3 text-2xl font-semibold uppercase tracking-wide text-white"
+      >Reset</button>
+    <button
+      aria-label="roll dice"
+      class="w-[7em] rounded-lg bg-[#8A2BE2] px-4 py-3 text-2xl font-semibold uppercase tracking-wide text-white"
+      >Roll</button>
   </div>
 </section>
