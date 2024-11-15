eab3a3aeeb3dc36b1debbe70e8bf08f63a8bb643
Ave Nurme <nurme.ave@gmail.com>
2024-11-15 15:26:53 +0200

    Add Google fonts, Font Awesome CDN link, configure Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
+  },
 
-	plugins: [typography]
+  plugins: [typography]
 };
