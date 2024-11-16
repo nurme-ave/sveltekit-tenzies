52c3a7715d69feaecbb96408992986fda26ab20e
Ave Nurme <nurme.ave@gmail.com>
2024-11-16 18:13:42 +0200

    Formatting: change "svelteBracketNewLine" value to 'true' in .prettierrc

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -6,6 +6,7 @@
   aria-label={ariaLabeltext}
   onclick={onClick}
   class="w-[6em] rounded-lg px-4 py-3 text-xl font-semibold uppercase tracking-wide text-white sm:w-[7em] sm:text-2xl"
-  style="background-color: {color}">
+  style="background-color: {color}"
+>
   {text}
 </button>
