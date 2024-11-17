798e8f223186297db756ee30999bdba2ad09143c
Ave Nurme <nurme.ave@gmail.com>
2024-11-17 14:24:22 +0200

    Replace h-dvh with min-h-dvh in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main
-  class="grid h-dvh place-content-center items-center bg-[#8A2BE2] font-poppins xsm:min-h-screen xsm:overflow-y-auto"
+  class="grid min-h-dvh place-content-center items-center bg-[#8A2BE2] font-poppins xsm:min-h-screen xsm:overflow-y-auto"
 >
   {@render children()}
 </main>
